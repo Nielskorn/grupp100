2988932a4bfd9247fc7c206daa582b850120b69c
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2022-02-25 10:14:55 +0100

    Messagesource

---
+++ LF8/src/main/java/utils/MessagesSource.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.dialect.AbstractHANADialect.HANABlobTypeDescriptor;
+
+import kotlin.reflect.jvm.internal.impl.load.java.BuiltinMethodsWithSpecialGenericSignature;
+import kotlin.reflect.jvm.internal.pcollections.HashPMap;
+
+public class MessagesSource {
+	
+	
+	static HashMap<String, String> hashdata= new HashMap<String,String>();
+	public static void createHashMap() {
+		hashdata.put("alert","this Message was send automaticly because");
+		hashdata.put("hardcapDisk"," is running out  it extends over ur set hardcap with: ");
+		hashdata.put("softcapDisk"," is becoming quit full handel it extens over ur set softcap with:");
+		hashdata.put("ram","ur ram usage extends over ur set caps with:" );
+		hashdata.put("Cpu", "ur cpu usage extends over ur set caps with:");
+		hashdata.put("bye", "ur monitioring Programm");
+		
+	}
+	public  static String getformHashmap(String key){
+	String value=	hashdata.get(key);
+	return value;
+	}
+	
+	
+}
