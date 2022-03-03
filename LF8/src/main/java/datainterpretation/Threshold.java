ac7bab3290712f668b5e7df406fa282d8997e06f
Marc Braumann <marcbra@gmx.de>
2022-03-03 09:35:07 +0100

    didnt push a class

---
+++ LF8/src/main/java/datainterpretation/Threshold.java
@@ -0,0 +1,13 @@
+package datainterpretation;
+
+public class Threshold {
+	
+	public String name;
+	public double value;
+	
+	public Threshold(String name, double value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+}
